5c137d1a8d29f616d71bee9afcf2960019eff63b
Adrian Hoehn <adrian.hoehn@minaxo.com>
2022-06-07 14:20:50 +0200

    nicer UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 import {Scanner} from "./components/scanner/Scanner"
+import {Footer} from "./components/footer/Footer";
+import Container from "./components/container/Container";
+import {Header} from "./components/heading/Header";
 
 function App() {
   return (
     <div className="App">
-      <Scanner/>
+        <Container>
+            <Header title="Scan your documents"/>
+            <div className="content">
+                <Scanner/>
+            </div>
+            <Footer/>
+        </Container>
     </div>
   );
 }
--- src/components/scanner/Scanner.tsx
+++ src/components/scanner/Scanner.tsx
@@ -1,17 +1,29 @@
 import Dynamsoft from "mobile-web-capture";
 import {WebTwain} from "mobile-web-capture/dist/types/WebTwain";
 import {useEffect} from "react";
+import Button from "../button/Button";
+import "./Scanner.css"
+import axios from "axios";
+
+export interface IMetadata {
+    fileName: string,
+    customerId: string,
+    consultantId: string,
+    processId: string
+}
 
 export const Scanner = () => {
+
     /* When using your own license, please uncomment the following lines and fill in your own information. */
     /* To get a free trial, please visit https://www.dynamsoft.com/customer/license/trialLicense?product=dwt. */
     Dynamsoft.DWT.ProductKey = "t0153KQMAAEnbkM/JlkQune9M7TfvczNx+qg1pvyv1xLRtwEKmsJHc9XF9fYatR9UqWgCr9fIYW5Rt8CIKAo92jZvXX6lSvQNZd8wDIOiIu+V6hQ6w4f5qJFm8FDnMvrWk/niuBjU6QxPGPGN9mdszMxnD9nO3BueMOIbM3Nn7D7nWP/xJ6CtZaaR4QkjvmmZ72YVFJK4/gHRnZ6I";
     Dynamsoft.DWT.ResourcesPath = "../mobile-web-capture";
-    Dynamsoft.DWT.Containers = [{ContainerId: 'dwtcontrolContainer', Width: 370, Height: 450}];
+    Dynamsoft.DWT.Containers = [{ContainerId: 'dwtcontrolContainer', Height: 200}];
     Dynamsoft.DWT.UseLocalService = false;
-    Dynamsoft.DWT.RegisterEvent('OnWebTwainReady', Dynamsoft_OnReady);
+    let DWObject: WebTwain;
 
     useEffect(() => {
+        Dynamsoft.DWT.RegisterEvent('OnWebTwainReady', ready);
         // @ts-ignore
         if (Dynamsoft.navInfoSync.bFileSystem && Dynamsoft.DWT.UseLocalService == false) {
             Dynamsoft.DWT.AutoLoad = false;
@@ -21,24 +33,12 @@ export const Scanner = () => {
         Dynamsoft.DWT.Load();
     }, [])
 
-    let DWObject: WebTwain;
-
-    return (
-        <div>
-            <div id="dwtcontrolContainer"></div>
-            <input type="button" value="scan document" onClick={ScanDocument}/>
-            <input type="button" value="Load" onClick={LoadImages}/>
-            <input type="button" value="SaveAsPDF" onClick={SaveAsPDF}/>
-            <input type="button" value="SaveAsTiff" onClick={SaveAsTiff}/>
-        </div>
-    );
-
-    function Dynamsoft_OnReady() {
+    const ready = () => {
         DWObject = Dynamsoft.DWT.GetWebTwain('dwtcontrolContainer');
         DWObject.Viewer.setViewMode(2, 2);
     }
 
-    function ScanDocument() {
+    const scanDocument = () => {
         var showVideoConfigs = {
             scannerViewer: {
                 autoDetect: {
@@ -63,7 +63,45 @@ export const Scanner = () => {
         }
     }
 
-    function LoadImages() {
+    const upload = () => {
+        if (DWObject) {
+            const indices = [];
+            if (DWObject.HowManyImagesInBuffer == 0) {
+                alert('There is no image in buffer.');
+                return;
+            }
+            for (let i = 0; i < DWObject.HowManyImagesInBuffer; i++){
+                indices.push(i);
+            }
+            const successCallback = (
+                result: Blob,
+                indices: number[],
+                type: number) => {
+                console.log('responseHandler', result, indices, type);
+                uploadBlob(result, {processId: 'TEST', fileName: 'test.pdf', customerId: 'ABC-123', consultantId: 'BCD-23423'})
+            }
+            const failureCallBack = (
+                errorCode: number,
+                errorString: string) => {
+                console.log('failureCallBack', errorCode, errorString)
+            }
+            const PDF = 4;
+            DWObject.ConvertToBlob( indices, PDF, successCallback, failureCallBack);
+        }
+    }
+
+    const uploadBlob =  (file: Blob, metadata: IMetadata) => {
+        let data = new FormData();
+        data.append('metadata', JSON.stringify(metadata));
+        data.append('file', file, metadata.fileName);
+        axios.post("/path/to/api", data, {
+            headers: {
+                "Content-Type": "multipart/form-data",
+            },
+        });
+    }
+
+    const loadImages = () => {
         if (DWObject) {
             if (DWObject.Addon && DWObject.Addon.PDF) {
                 DWObject.Addon.PDF.SetResolution(300);
@@ -79,7 +117,7 @@ export const Scanner = () => {
         }
     }
 
-    function SaveAsPDF() {
+    const saveAsPDF = () => {
         if (DWObject) {
             if (DWObject.HowManyImagesInBuffer == 0) {
                 alert('There is no image in buffer.');
@@ -89,7 +127,7 @@ export const Scanner = () => {
         }
     }
 
-    function SaveAsTiff() {
+    const saveAsTiff = () => {
         if (DWObject) {
             if (DWObject.HowManyImagesInBuffer == 0) {
                 alert('There is no image in buffer.');
@@ -98,4 +136,16 @@ export const Scanner = () => {
             DWObject.SaveAllAsMultiPageTIFF("webTWAINImage.tiff");
         }
     }
+
+    return (
+        <div>
+            <div id="dwtcontrolContainer"></div>
+            <div className="controlbar">
+                {/* @ts-ignore */}
+                <Button onClick={scanDocument}>Scan</Button>
+                {/* @ts-ignore */}
+                <Button onClick={upload}>Upload</Button>
+            </div>
+        </div>
+    );
 }
